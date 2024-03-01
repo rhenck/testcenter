0970d817f1c63fa5b878788ae4903d4d14eaa3ee
rhenck <richard.henck@iqb.hu-berlin.de>
2024-03-01 13:25:56 +0100

    [fe] Redesign login page
    - Integrate the 2 panels into 1
    - Remove link to SysCheck. This is not something that needs to be 
    available to the general user. It can be accessed by entering the URL 
    directly by people who need it.
    - Move Impressum to footer bar

--- frontend/src/app/app-root/login/login.component.ts
+++ frontend/src/app/app-root/login/login.component.ts
@@ -11,12 +11,7 @@ import browsersJson from '../../../../../definitions/browsers.json';
 
 @Component({
   templateUrl: './login.component.html',
-  styles: [
-    '.mat-mdc-form-field {display: block}',
-    '.mat-mdc-card {width: 400px;}',
-    '.version-label {position: fixed; bottom: 0; right: 0; background: rgba(255,255,255, 0.3); padding: 0.5em 1em}',
-    '.version-label a {color:black; text-decoration: none; font-size: large}'
-  ]
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit, OnDestroy {
